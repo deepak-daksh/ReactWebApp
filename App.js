22f5ea8d96dba3daa8b315075abf7753152eb274
Deepak Kumar Singh <deepak.dgnr@gmail.com>
2026-03-29 21:19:27 +0530

    episode 2 commit

--- App.js
+++ App.js
@@ -1,3 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
 /* 
 <div id="parent">
     <div id="child">
@@ -14,16 +18,16 @@
 
 const parent = React.createElement("div",{id:"parent"},
     [
-        React.createElement("div",{id:"child"},
+        React.createElement("div",{id:"child", key:"child"},
         [
-            React.createElement("h1",{},"I'm h1 tag"), 
-            React.createElement("h1",{},"I'm h2 tag")
+            React.createElement("h1",{key:"child-h1-1"},"I'm h1 tag"), 
+            React.createElement("h1",{key:"child-h1-2"},"I'm h2 tag")
         ]
     ),
-    React.createElement("div",{id:"child2"},
+    React.createElement("div",{id:"child2", key:"child2"},
         [
-            React.createElement("h1",{},"I'm h1 tag"), 
-            React.createElement("h1",{},"I'm h2 tag")
+            React.createElement("h1",{key:"child2-h1-1"},"I'm h1 tag"), 
+            React.createElement("h1",{key:"child2-h1-2"},"I'm h2 tag")
         ]
     )
     ]
@@ -34,6 +38,6 @@ const parent = React.createElement("div",{id:"parent"},
 //     "h1",
 //     {id:"heading", alt:"Hello world from React !"},
 //     "Hello world from React !");
-console.log(parent);
+// console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent);
